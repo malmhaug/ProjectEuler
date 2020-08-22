3be736326679c7b9e4b78797efcb9687e7245660
Jim-Kristian Malmhaug <malmhaug@gmail.com>
2020-08-22 20:29:28 +0200

    Updated solution for ID1. Added solution for ID2

--- ID1/id1.py
+++ ID1/id1.py
@@ -1,6 +1,7 @@
 """
-Problem: If we list all natural numbers below 10 that are multiplies of 3 or 5, we get [3,5,9].
-         The sum of these multiplies is 23. Find the sum of all the multiplies of 3 or 5 below 1000.
+Problem: Using only natural numbers, find the sum of all the multiplies of 3 or 5 below 1000.
+
+Example: Using 10 as a limit, all multiplies of 3 or 5 below this limit is [3,5,9]. The sum of these multiplies is 23.
 
 Natural number: Positive integers, like 0,1,2,3,4,etc....
 """
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Tasks:
 
 ID1 - Multiples of 3 and 5 | Execution time: ~0.0 ms
 
-ID2 - Even Fibonacci numbers | Execution time: 
+ID2 - Even Fibonacci numbers | Execution time: ~0.0 ms
+
